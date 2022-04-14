b02180b44580ac23d70aabb00359175b2597e042
stlef14 <stlef14@users.noreply.github.com>
2022-04-14 09:23:54 -0400

    validates no new (#21)

--- terraform/azure/aks-slef2.tf
+++ terraform/azure/aks-slef2.tf
@@ -215,4 +215,5 @@ resource azurerm_kubernetes_cluster "k8s_6_cluster" {  # noboost
      git_repo             = "terragoat" 
      yor_trace            = "6103d111-864e-42e5-899c-1864de281fd1" 
    } 
- } 
+ }
+ # adds comments
